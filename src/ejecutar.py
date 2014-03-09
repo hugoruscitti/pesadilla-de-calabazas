12f7244028f8646a68a9af3a6e3310e2aeeb3277
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-03-09 00:48:01 -0300

    Creando al personaje principal.

--- src/ejecutar.py
+++ src/ejecutar.py
@@ -1,11 +1,32 @@
 import pilas
-pilas.iniciar()
+pilas.iniciar(ancho=800, alto=600)
 pilas.reiniciar_si_cambia(__file__)
 
-pilas.actores.Pelota() * 3
-pilas.fondos.Tarde()
 
-aceituna = pilas.actores.Aceituna()
-aceituna.escala = 4
+class Protagonista(pilas.actores.Actor):
+
+    def __init__(self):
+        self.grilla = pilas.imagenes.cargar_grilla('../data/protagonista.png', 6)
+        pilas.actores.Actor.__init__(self)
+        self.imagen = self.grilla
+        self.y = -200
+
+    def actualizar(self):
+        velocidad = 10
+
+        if pilas.mundo.control.izquierda:
+            self.x -= velocidad
+
+        if pilas.mundo.control.derecha:
+            self.x += velocidad
+
+        if self.x < -400:
+            self.x = -400
+
+        if self.x > 400:
+            self.x = 400
+
+
+Protagonista()
 
 pilas.ejecutar()
