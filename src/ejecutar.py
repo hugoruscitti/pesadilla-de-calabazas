bce49a3bd41bd9406a8b9c40f1192c967673aa1b
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-03-11 10:13:06 -0300

    Implementando estados, el fondo de pantalla y el actor sombra.

--- src/ejecutar.py
+++ src/ejecutar.py
@@ -3,6 +3,68 @@ pilas.iniciar(ancho=800, alto=600)
 pilas.reiniciar_si_cambia(__file__)
 
 
+class Sombra(pilas.actores.Actor):
+
+    def __init__(self, actor):
+        pilas.actores.Actor.__init__(self)
+        self.imagen = "../data/sombra.png"
+        self.actor_a_seguir = actor
+        self.transparencia = 50
+        self.z = actor.z + 1
+
+    def actualizar(self):
+        self.x = self.actor_a_seguir.x
+        self.y = self.actor_a_seguir.y
+
+
+class Accion:
+
+    def iniciar(self, actor):
+        pass
+    def actualizar(self, actor):
+        actor.rotacion = 130
+        pass
+
+class Caminando(Accion):
+
+    def iniciar(self, actor):
+        print "Ingresando al modo Caminando"
+        pass
+
+    def actualizar(self, actor):
+        velocidad = 10
+
+        if pilas.mundo.control.izquierda:
+            actor.x -= velocidad
+            actor.espejado = True
+
+        if pilas.mundo.control.derecha:
+            actor.x += velocidad
+            actor.espejado = False
+
+        self.aplicar_limites_del_escenario(actor)
+
+        if not pilas.mundo.control.izquierda and not pilas.mundo.control.derecha:
+            actor.realizar_accion(Parado())
+
+    def aplicar_limites_del_escenario(self, actor):
+        if actor.x < -400:
+            actor.x = -400
+
+        if actor.x > 400:
+            actor.x = 400
+
+class Parado(Accion):
+
+    def iniciar(self, actor):
+        print "Ingresando al modo Parado"
+        pass
+
+    def actualizar(self, actor):
+        if pilas.mundo.control.izquierda or pilas.mundo.control.derecha:
+            actor.realizar_accion(Caminando())
+
+
 class Protagonista(pilas.actores.Actor):
 
     def __init__(self):
@@ -10,23 +72,19 @@ class Protagonista(pilas.actores.Actor):
         pilas.actores.Actor.__init__(self)
         self.imagen = self.grilla
         self.y = -200
+        self.realizar_accion(Parado())
 
     def actualizar(self):
-        velocidad = 10
+        self.accion.actualizar(self)
 
-        if pilas.mundo.control.izquierda:
-            self.x -= velocidad
-
-        if pilas.mundo.control.derecha:
-            self.x += velocidad
-
-        if self.x < -400:
-            self.x = -400
-
-        if self.x > 400:
-            self.x = 400
+    def realizar_accion(self, accion):
+        accion.iniciar(self)
+        self.accion = accion
 
 
-Protagonista()
+print "iniciando", Parado
+pilas.fondos.Fondo('../data/fondo.png')
+protagonista = Protagonista()
+Sombra(protagonista)
 
 pilas.ejecutar()
