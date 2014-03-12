e989322e38df15a1042d87573e67942a074231c1
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-03-12 00:52:19 -0300

    Agregando calabazas que se pueden eliminar cuando se tocan.

--- src/ejecutar.py
+++ src/ejecutar.py
@@ -14,21 +14,28 @@ class Sombra(pilas.actores.Actor):
 
     def actualizar(self):
         self.x = self.actor_a_seguir.x
-        self.y = self.actor_a_seguir.y
+        self.y = self.actor_a_seguir.y - self.actor_a_seguir.altura_salto
+        self.escala = 1 - (self.actor_a_seguir.altura_salto / 500.0)
 
 
 class Accion:
 
     def iniciar(self, actor):
         pass
+
     def actualizar(self, actor):
-        actor.rotacion = 130
         pass
+
+    def aplicar_limites_del_escenario(self, actor):
+        if actor.x < -400:
+            actor.x = -400
+
+        if actor.x > 400:
+            actor.x = 400
 
 class Caminando(Accion):
 
     def iniciar(self, actor):
-        print "Ingresando al modo Caminando"
         pass
 
     def actualizar(self, actor):
@@ -47,23 +54,46 @@ class Caminando(Accion):
         if not pilas.mundo.control.izquierda and not pilas.mundo.control.derecha:
             actor.realizar_accion(Parado())
 
-    def aplicar_limites_del_escenario(self, actor):
-        if actor.x < -400:
-            actor.x = -400
+        if pilas.mundo.control.arriba:
+            actor.realizar_accion(Saltando())
 
-        if actor.x > 400:
-            actor.x = 400
 
 class Parado(Accion):
 
     def iniciar(self, actor):
-        print "Ingresando al modo Parado"
         pass
 
     def actualizar(self, actor):
         if pilas.mundo.control.izquierda or pilas.mundo.control.derecha:
             actor.realizar_accion(Caminando())
 
+        if pilas.mundo.control.arriba:
+            actor.realizar_accion(Saltando())
+
+class Saltando(Accion):
+
+    def iniciar(self, actor):
+        self.velocidad_salto = 20
+        self.y = actor.y
+
+    def actualizar(self, actor):
+        velocidad = 7
+        self.velocidad_salto -= 1
+        actor.y += self.velocidad_salto
+
+        if self.y > actor.y:
+            actor.y = self.y
+            actor.realizar_accion(Parado())
+
+        if pilas.mundo.control.izquierda:
+            actor.x -= velocidad
+            actor.espejado = True
+
+        if pilas.mundo.control.derecha:
+            actor.x += velocidad
+            actor.espejado = False
+
+        actor.altura_salto = actor.y - self.y
 
 class Protagonista(pilas.actores.Actor):
 
@@ -73,6 +103,8 @@ class Protagonista(pilas.actores.Actor):
         self.imagen = self.grilla
         self.y = -200
         self.realizar_accion(Parado())
+        self.altura_salto = 0
+        self.radio_de_colision = 50
 
     def actualizar(self):
         self.accion.actualizar(self)
@@ -81,10 +113,27 @@ class Protagonista(pilas.actores.Actor):
         accion.iniciar(self)
         self.accion = accion
 
+class Calabaza(pilas.actores.Actor):
 
-print "iniciando", Parado
+    def __init__(self, x=0, y=0):
+        imagen = "../data/calabaza.png"
+        pilas.actores.Actor.__init__(self, imagen, x, y)
+        figura = pilas.fisica.Circulo(x, y, 70)
+        self.imitar(figura)
+        self.radio_de_colision = 70
+
+
 pilas.fondos.Fondo('../data/fondo.png')
 protagonista = Protagonista()
 Sombra(protagonista)
+calabazas = Calabaza() * 2
+
+
+def cuando_toca_calabaza(protagonista, calabaza):
+    calabaza.eliminar()
+
+
+pilas.mundo.colisiones.agregar(protagonista, calabazas, cuando_toca_calabaza)
+
 
 pilas.ejecutar()
